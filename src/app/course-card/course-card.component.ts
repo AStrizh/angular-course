bafa6dc971011d410517bbfb6cf64cf51c09e862
Aleksandr Strizhevskiy <abstrizhevskiy@yahoo.com>
2025-06-10 20:45:59 -0400

    Started learning project

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,29 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Course} from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+  @Input({
+    required:true
+  })
+  course: Course;
+
+  @Input({required:true})
+  index: number;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+
+  onCourseViewed() {
+      console.log("Card component - button clicked");
+
+      this.courseSelected.emit(this.course);
+  }
+
+}
